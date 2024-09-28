0234cf5cdc6dd94d4e330c006e7ac07988d8f383
TOBENTOch <info@tobento.ch>
2024-09-28 08:46:15 +0200

    id added

--- src/Role.php
+++ src/Role.php
@@ -21,5 +21,47 @@ use Tobento\Service\Acl\Role as BaseRole;
  */
 class Role extends BaseRole implements RoleInterface
 {
-    //
+    /**
+     * Create a new Role
+     *
+     * @param string|int $id,
+     * @param string $key A role key such as 'editor'.
+     * @param array $areas The areas for the role ['frontend', 'api'].
+     * @param bool $active If the role is active.
+     * @param null|string $name A role name such as 'Editor'.
+     */    
+    public function __construct(
+        protected string|int $id,
+        protected string $key,
+        protected array $areas = ['frontend'],
+        protected bool $active = true,
+        protected null|string $name = null,
+    ) {}
+    
+    /**
+     * Get the id.
+     *
+     * @return int|string
+     */
+    public function id(): string|int
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Object to array
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id(),
+            'key' => $this->key(),
+            'name' => $this->name(),
+            'active' => $this->active(),
+            'areas' => $this->areas(),
+            'permissions' => $this->getPermissions(),
+        ];
+    }
 }
