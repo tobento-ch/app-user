d639e73be25811708f10e83f531b38e632f58376
TOBENTOch <info@tobento.ch>
2025-03-08 14:04:11 +0100

    handling InvalidateTokenException

--- src/Boot/HttpUserErrorHandler.php
+++ src/Boot/HttpUserErrorHandler.php
@@ -13,17 +13,20 @@ declare(strict_types=1);
  
 namespace Tobento\App\User\Boot;
 
+use Psr\Http\Message\ResponseInterface;
 use Tobento\App\Http\Boot\ErrorHandler;
-use Tobento\App\User\Exception\TokenExpiredException;
+use Tobento\App\User\Authentication\Token\TokenStorageInterface;
+use Tobento\App\User\Authentication\Token\TokenTransportInterface;
 use Tobento\App\User\Exception\AuthenticationException;
 use Tobento\App\User\Exception\AuthorizationException;
+use Tobento\App\User\Exception\InvalidateTokenException;
 use Tobento\App\User\Exception\PermissionDeniedException;
 use Tobento\App\User\Exception\RoleDeniedException;
+use Tobento\App\User\Exception\TokenExpiredException;
 use Tobento\Service\Requester\RequesterInterface;
 use Tobento\Service\Responser\ResponserInterface;
 use Tobento\Service\Routing\RouterInterface;
 use Tobento\Service\Routing\UrlException;
-use Psr\Http\Message\ResponseInterface;
 use Throwable;
 
 class HttpUserErrorHandler extends ErrorHandler
@@ -45,6 +48,23 @@ class HttpUserErrorHandler extends ErrorHandler
             return $requester->wantsJson()
                 ? $this->renderJson(code: 403, message: $this->getMessage(419))
                 : $this->renderView(code: 403, message: $this->getMessage(419));
+        }
+        
+        if ($t instanceof InvalidateTokenException) {
+            $tokenStorage = $this->app->get(TokenStorageInterface::class);
+            $tokenTransport = $this->app->get(TokenTransportInterface::class);
+            
+            $response = $requester->wantsJson()
+                ? $this->renderJson(code: 403, message: $this->getMessage(419))
+                : $this->renderView(code: 403, message: $this->getMessage(419));
+            
+            $tokenStorage->deleteToken($t->token());
+            
+            return $tokenTransport->removeToken(
+                token: $t->token(),
+                request: $requester->request(),
+                response: $response,
+            );
         }
         
         if ($t instanceof AuthenticationException) {
